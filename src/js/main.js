fcfce1f1f2ed694a75be5a3888c4a1cdab2bbaa9
Colene McFarlane <cmcfarlane22@gmail.com>
2022-07-07 09:50:26 -0400

    Initialize AOS animations, configure 'More Posts' to load more posts, and add descriptive comments

--- src/js/main.js
+++ src/js/main.js
@@ -1,64 +1,74 @@
 import 'bootstrap';
+import AOS from 'aos';
 
 const apiURL                = "https://mockend.com/Wide-Eye-Creative/technical-test-2022/posts";
-const archivePostsTemplate  = require( '../partials/single-post-item.hbs' );
-const archivePostsContainer = document.querySelector( '.posts__container' );
+const archivePostsTemplate  = require('../partials/single-post-item.hbs');
+const archivePostsContainer = document.querySelector('.posts__container');
 
 let allPosts          = null;
 let postsSubset       = null;
 let sortedPostsSubset = null;
 let previousPostId    = 0;
-// let loadMorePosts        = document.querySelector( '.load-more-posts' );
+let loadMorePosts     = document.querySelector('.load-more-posts');
 
-/**
- * TODO:
- * 1. add hover effect for mobile menu icon
- * 2. hookup event listener to load more posts when link is clicked if time permits (must append)
- * 3. make sure read more/more posts redirect to google.com
- * 4. use title as img alt text
- */
+// initialize scroll/fade in animations
+AOS.init({
+  duration: 1200,
+});
 
-fetch( apiURL )
-  .then( ( response ) => {
+// get all posts
+fetch(apiURL)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Unsuccessful fetch attempt');
+    }
     return response.json();
   })
-  .then( ( data ) => {
+  .then((data) => {
     allPosts          = data;
-    postsSubset       = getPostSubset( allPosts, previousPostId );
-    sortedPostsSubset = getSortedPostsSubset( postsSubset );
+    postsSubset       = getPostSubset(allPosts, previousPostId);
+    sortedPostsSubset = getSortedPostsSubset(postsSubset);
 
-    renderHTML( sortedPostsSubset );
+    renderHTML(sortedPostsSubset);
+  })
+  .catch(error => {
+    console.error('An unexpected error occurred:', error);
   });
 
-function renderHTML( posts ) {
-  // compile and render HTML using handlebars template partial
-  archivePostsContainer.insertAdjacentHTML( 'beforeend', archivePostsTemplate( posts ) );
+// compile and render HTML using handlebars template partial
+function renderHTML(posts) {
+  archivePostsContainer.insertAdjacentHTML('beforeend', archivePostsTemplate(posts));
 }
 
-// Get six posts at a time based on last index position
-function getPostSubset( posts, index ) {
+// retrieve six posts at a time based on last index position
+function getPostSubset(posts, index) {
   previousPostId += 6;
-  let subset = allPosts.slice( index, index + 6, posts );
+  let subset = allPosts.slice(index, index + 6, posts);
 
   // change date format to Month Day, Year ( March 05, 2011 )
-  changePusblishDateFormat( subset );
+  changePusblishDateFormat(subset);
   return subset;
 }
 
-function getSortedPostsSubset( posts ) {
-  return posts.sort( ( a, b ) => ( a.authorName.toUpperCase() < b.authorName.toUpperCase() ) ? -1 : 
-    ( a.authorName.toUpperCase() > b.authorName.toUpperCase() ) ? 1 : 0 );
+// sort posts based on author name
+function getSortedPostsSubset(posts) {
+  return posts.sort((a, b) => (a.authorName.toUpperCase() < b.authorName.toUpperCase()) ? -1 :
+    (a.authorName.toUpperCase() > b.authorName.toUpperCase()) ? 1 : 0);
 }
 
-function changePusblishDateFormat( posts ) {
-  posts.forEach( post => {
-    const publishedDate = new Date( post.publishedDate );
+// set published date format to Month Day, Year format
+function changePusblishDateFormat(posts) {
+  posts.forEach(post => {
+    const publishedDate     = new Date(post.publishedDate);
     const dateFormatOptions = { month: 'long', day: 'numeric', year: 'numeric' };
-    post.publishedDate = publishedDate.toLocaleDateString( 'en-us', dateFormatOptions );
+    post.publishedDate      = publishedDate.toLocaleDateString('en-us', dateFormatOptions);
   });
 }
 
-// loadMorePosts.addEventListener( 'click', () => {
-  // subPostCollection = getSubPostCollection( allPosts, postIndex );
-  //some other stuff needs to happen
-// });
+// load additional posts in sorted order
+loadMorePosts.addEventListener('click', () => {
+  postsSubset       = getPostSubset(allPosts, previousPostId);
+  sortedPostsSubset = getSortedPostsSubset(postsSubset);
+
+  renderHTML(sortedPostsSubset);
+});
